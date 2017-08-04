ee1ff2c514434dc494450dcedc4dff74d4c10fe2
Kojak420 <klkovacs86@gmail.com>
2017-08-04 19:27:36 -0400

    Perfection

--- FirstApplication/Models/IdentityModels.cs
+++ FirstApplication/Models/IdentityModels.cs
@@ -16,7 +16,8 @@ namespace FirstApplication.Models
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
+            // Add 
+           // user claims here
             return userIdentity;
         }
 
